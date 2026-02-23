2fde1be0296bb859c86ff1522b14ba58b20a96da
J-Moessmer <257735484+J-Moessmer@users.noreply.github.com>
2026-02-23 13:44:56 +0100

    Create 08_chargeing.py

---
+++ 26-02-23/08_chargeing.py
@@ -0,0 +1,20 @@
+#26-02-23_08_chargeing
+class handy:
+    def __init__(self, brand, charge):
+        self.brand = brand
+        self.charge = charge
+        charge = 0
+               
+
+    def info(self):
+        for i in range(0,100,1):
+            self.charge = self.charge +1
+            if self.charge <= 101 and self.charge%10==0:
+                print(f"{self.brand} charge level: {self.charge}%")
+            else:
+                pass#print("else",self.charge)
+
+C1 = handy("Nokia", 55)
+
+
+C1.info()
